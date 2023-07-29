30580981f923dfeb5c090c122b38fddd6a6d5ac6
YanuarEka <yanuarekasusila@gmail.com>
2023-07-29 21:53:49 +0700

    adding component,styling and testing for InputDate & Breadcrumb

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 
-import InputNumber from 'elements/Form/InputNumber';
+import Breadcrumb from 'elements/Breadcrumb';
 
 export default class Example extends Component {
-  state = {
-    value: '1',
-  };
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
   render() {
+    const breadcrumblist = [
+      { pageTitle: 'Home', pageHref: '' },
+      { pageTitle: 'House Details', pageHref: '' },
+    ];
+
     return (
       <div className="container">
         <div className="row align-items-center justify-content-center" style={{ height: '100vh' }}>
           <div className="col-auto">
-            <InputNumber max={30} suffix=" night" isSuffixPlural onChange={this.handleChange} name="value" value={this.state.value} />
+            <Breadcrumb data={breadcrumblist} />
           </div>
         </div>
       </div>
