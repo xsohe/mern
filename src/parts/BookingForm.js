6b3c754912307e6d40625bcfbe6f7c771d85d5ce
YanuarEka <yanuarekasusila@gmail.com>
2023-08-14 21:22:49 +0700

    Adding connect item detail to checkout booking page

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -59,6 +59,19 @@ export default class BookingForm extends Component {
       });
     }
   }
+
+  startBooking = () => {
+    const { data } = this.state;
+    this.props.startBooking({
+      _id: this.props.itemDetails._id,
+      duration: data.duration,
+      date: {
+        startDate: data.date.startDate,
+        endDate: data.date.endDate,
+      },
+    });
+  };
+
   render() {
     const { data } = this.state;
     const { itemDetails, startBooking } = this.props;
@@ -83,7 +96,7 @@ export default class BookingForm extends Component {
           </span>
         </h6>
 
-        <Button className="btn" hasShadow isPrimary isBlock onClick={startBooking}>
+        <Button className="btn" hasShadow isPrimary isBlock onClick={this.startBooking}>
           Continue to Book
         </Button>
       </div>
